bb4de963cc24d0e91aac002697938bbdc283787d
oanikienko <ouliana.anikienko@gmail.com>
2023-11-25 15:33:32 +0100

    Adds tests for initial circuit and folding

--- src/circuits/folding.py
+++ src/circuits/folding.py
@@ -1,11 +1,12 @@
 # Module: Folding - repeat gates unitarily to boost noise in the circuit
 #         Implements the following methods: global folding, local folding
-# Date: 22/11/2023
+# Date: 23/11/2023
 
 
 ## == Libraries == ##
 
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from circuits import initial_circuits as init
 
 ## == Functions == ##
 
@@ -23,19 +24,13 @@ def global_folding(initial_circuit, nb_qubits, noise_factor):
 
 def local_folding(initial_circuit, nb_qubits, noise_factor):
 
-    # print(initial_circuit)
     q_register = QuantumRegister(nb_qubits, name='q')
 
     folded_circuit = QuantumCircuit(q_register)
     
     circuit_instructions = []
     for circuit_instruction in initial_circuit.data:
-        # print("Operation: ", circuit_instruction.operation)
-        # print("Qubits: ", circuit_instruction.qubits)
-        # print("Cbits: ", circuit_instruction.clbits)
         circuit_instructions.append(circuit_instruction)
-
-    # print(circuit_instructions)
 
     for i in range(0,len(circuit_instructions)):
         circuit_instruction = circuit_instructions[i]
@@ -51,4 +46,28 @@ def local_folding(initial_circuit, nb_qubits, noise_factor):
 
 ## == Tests == ##
 
+if __name__ == "__main__":
+
+    nb_qubits = 4
+    noise_factors = [1,3,5]
+
+    print(">> Initial circuit")
+    initial_circuit = init.build_initial_circuit(nb_qubits, init.U_4qubits)
+    print(initial_circuit)
+
+    print(">> Global folding")
+    folded_circuits = []
+    for factor in noise_factors[1:]:
+        folded_circuits.append(global_folding(initial_circuit, nb_qubits, factor))
+
+    for i in range(len(folded_circuits)):
+        print(folded_circuits[i])
+
+    print(">> Local folding")
+    folded_circuits = []
+    for factor in noise_factors[1:]:
+        folded_circuits.append(local_folding(initial_circuit, nb_qubits, factor))
+
+    for i in range(len(folded_circuits)):
+        print(folded_circuits[i])
 
